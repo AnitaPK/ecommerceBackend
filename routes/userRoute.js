4839a1b47762927e44684f343a5904ce8b0a98f5
AnitaPK <anita.khamitkar@gmail.com>
2024-08-14 17:47:17 +0530

    files added.

---
+++ routes/userRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+
+const router = express.Router();
+
+router.post('/register', userController.registerUser);
+router.post('/login', userController.loginUser);
+
+module.exports = router;
+
+
+//http://localhost:5000/api/auth/register
+//http://localhost:5000/api/auth/login
